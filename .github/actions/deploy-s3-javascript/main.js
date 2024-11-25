663ca653f9e0274a8134b3e28c351b82444696f4
framanna <francisco.mansilla.navarro@gmail.com>
2024-11-25 13:20:57 +0100

    S3 bucket logic

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,19 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some input values
+
+    // We need to create a S3 bucket, with read permissions, enable the website hosting
+    const bucket = core.getInput('bucket', { required: true});
+    const bucketRegion = core.getInput('bucket-region', { required: true});
+    const distFolder = core.getInput('dist-folder', { required: true});
+
+
+    // 2) Upload my files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+
     core.notice('Hello from my custom Javascript action')
 }
 run();

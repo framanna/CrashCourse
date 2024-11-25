98b2c316b25c87e2f8ce5f9659e9906d623101ed
framanna <francisco.mansilla.navarro@gmail.com>
2024-11-25 12:05:24 +0100

    change duplicate core variable

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-const core = require('@actions/exec')
+const exec = require('@actions/exec')
 
 function run() {
     core.notice('Hello from my custom Javascript action')
